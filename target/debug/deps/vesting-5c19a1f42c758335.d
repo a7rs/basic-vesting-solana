8d1ccfeb822b0dd9bee2ec0238999f4daee1df61
a <a@serfrae.com>
2022-03-16 22:43:53 +0800

    repo cleanup

--- target/debug/deps/vesting-5c19a1f42c758335.d
+++
@@ -1,5 +0,0 @@
-/home/a7rs/projects/scy/vesting/target/debug/deps/vesting-5c19a1f42c758335.rmeta: programs/vesting/src/lib.rs
-
-/home/a7rs/projects/scy/vesting/target/debug/deps/vesting-5c19a1f42c758335.d: programs/vesting/src/lib.rs
-
-programs/vesting/src/lib.rs:
--- target/debug/deps/vesting-5f181965fb255304.d
+++
@@ -1,5 +0,0 @@
-/home/a7rs/projects/scy/vesting/target/debug/deps/vesting-5f181965fb255304.rmeta: programs/vesting/src/lib.rs
-
-/home/a7rs/projects/scy/vesting/target/debug/deps/vesting-5f181965fb255304.d: programs/vesting/src/lib.rs
-
-programs/vesting/src/lib.rs:
